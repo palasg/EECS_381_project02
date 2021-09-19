30007fae21693a647a033b7a6d70fd636b36d729
palasg <palas1991@gmail.com>
2021-09-19 16:28:01 +0530

    release build issue resolved

--- build-all.sh
+++ build-all.sh
@@ -20,22 +20,15 @@ callcmake(){
 
     if [[ $# -eq 1 ]]; then
 
-        echo "Its not a clean build"
-
         if [[ $1 == "Debug" ]]; then
-            echo "Debug build"
-            echo ${cmakerootdir}
-            echo ${build_dir}
             cmake -S ${cmakerootdir} -DCMAKE_BUILD_TYPE=$1 -B ${build_dir}
+            cmake --build ${build_dir}
         else
-         echo "Release build"
-         echo ${cmakerootdir}
-            echo ${build_dir}
-            cmake -s ${cmakerootdir} -DCMAKE_BUILD_TYPE=$1 -b ${build_dir}
+            cmake -S ${cmakerootdir} -DCMAKE_BUILD_TYPE=$1 -B ${build_dir}
         fi
 
     else
-        echo "its a clean build "
+        echo "Starting a clean build "
 
         if [[ $1 == "Debug" ]]; then
             cmake -S ${cmakerootdir} -DCMAKE_BUILD_TYPE=$1  -B ${build_dir}
@@ -85,17 +78,12 @@ if [[ ${build_flag} != "Debug" ]]; then
     fi
 fi
 
-
-
     if [[ ${clean_build_flag} == true ]]
     then
-        echo "clean build flag"
-
         createfolders ${build_flag}
         callcmake ${build_flag} ${clean_build_flag}
 
     else
-        echo "no clean build flag"
         createfolders ${build_flag}
         callcmake ${build_flag}
     fi
